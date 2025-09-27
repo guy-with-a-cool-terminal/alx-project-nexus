1691aed987c6a4735433e0c9891e7016e4ede3fd
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-09-27 09:11:17 +0300

    test:making migrations

--- ecommerce-backend/app/store/dev.py
+++
@@ -1,9 +0,0 @@
-# In MIDDLEWARE - add at the top
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # ... rest of your middleware
-]
-
-# Static files configuration
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
